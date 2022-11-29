84df5bbfdbe6dfa80efb38826773ba647b0f90f2
Matt Bartholomew <matthew.b.bartholomew@gmail.com>
2022-11-29 14:49:06 -0500

    debugging

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function compute()
     var rate=document.getElementById("rate").value;
     var years=document.getElementById("years").value;
     var interest=principal*years*rate/100;
+    var amount=interest+principal;
     var year=new Date().getFullYear()+parseInt(years); 
     //p = document.getElementById("principal").value;
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
