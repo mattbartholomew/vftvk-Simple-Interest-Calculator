d1cf0446340cf138fde643d816aa5cf42089199d
Matt Bartholomew <matthew.b.bartholomew@gmail.com>
2022-11-29 13:20:36 -0500

    edited the script file

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function compute()
     var interest=principal*years*rate/100;
     var year=new Date().getFullYear()+parseInt(years); 
     //p = document.getElementById("principal").value;
-    //document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"   
+//document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"   
